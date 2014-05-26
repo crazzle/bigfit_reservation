b50d35f75fafd978167a287054a3050d31704fad
Mark K <mark.keinhoerster@gmail.com>
2014-05-26 20:23:51 +0200

    verweis auf reservierungssystem nach login

--- bigfit_reservation/includes/process_login.php
+++ bigfit_reservation/includes/process_login.php
@@ -10,7 +10,7 @@ if (isset ( $_POST ['email'], $_POST ['p'] )) {
 	
 	if (login ( $email, $password, $mysqli ) == true) {
 		// Login erfolgreich
-		header ( 'Location: ../protected_page.php' );
+		header ( 'Location: ../reservationsystem/protected_page.php' );
 	} else {
 		// Login fehlgeschlagen
 		header ( 'Location: ../index.php?error=1' );

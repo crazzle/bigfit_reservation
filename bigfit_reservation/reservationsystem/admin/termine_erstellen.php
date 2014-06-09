2c3dc8ecc87549c4e2298bdfe6fdfacab1b6c5b2
superkeese <thedaab@googlemail.com>
2014-06-09 20:11:20 +0200

    termine erstellen TDA

--- bigfit_reservation/reservationsystem/admin/termine_erstellen.php
+++ bigfit_reservation/reservationsystem/admin/termine_erstellen.php
@@ -15,254 +15,29 @@ include_once '../../includes/functions.php';
 </head>
 <body>
 	<h1>Neue Trainingstermine anlegen</h1>
-	<table>
+
 	<?php
-	
-	for($count = 1; $count <= 7; $count ++) {
-		echo "<tr>";
-		echo "<th>";
-		echo "<input type='checkbox' name='weekDay" . $count . "' value='" . $count . "' /></th><th>".date("l", mktime(0, 0, 0, 7, 1, 2000))."</th>";
-		echo "<tr>";
-		for($countDays = 1; $countDays <= 15; $countDays ++){
-			echo "<td><input type='checkbox' name='dayHour'".$countDays."' value='".$countDays."' /></td>";
-			echo "<td>9:00</td>";
+	setlocale(LC_TIME, "de_DE");
+	$tage = array("Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag");
+	echo "<table>";
+	echo "<tr>";
+		for($countDays = 0; $countDays <= 6; $countDays ++)
+		{
+			echo "<td><input type='checkbox' name='day'".$tage[$countDays]."' value='".$tage[$countDays]."' />".$tage[$countDays]."</td>";
+			echo "<tr>";
+			echo "<td></td>";
+			for($countHours= 10; $countHours <= 21; $countHours ++)
+			{
+				echo "<td><input type='checkbox' name='hour'".$tage[$countDays].$countHours."' value='".$tage[$countDays].$countHours."':00' />".$countHours.":00</td>";			
+			}
+			echo "</tr>";
+			//echo "<br />";
 		}
-		echo "</tr>";
-	}
 	
+	echo "</tr>";
+	echo "</table>";
 	?>
-  	<tr>
-			<th><input type="checkbox" name="cMo" value="1" /></th>
-			<th>Montag</th>
-			<td></td>
-			<td><input type="checkbox" name="cT1" value="2" /></td>
-			<td>9:00</td>
-			<td><input type="checkbox" name="cT2" value="3" /></td>
-			<td>10:00</td>
-			<td><input type="checkbox" name="cT3" value="4" /></td>
-			<td>11:00</td>
-			<td><input type="checkbox" name="cT4" value="5" /></td>
-			<td>12:00</td>
-			<td><input type="checkbox" name="cT5" value="6" /></td>
-			<td>13:00</td>
-			<td><input type="checkbox" name="cT6" value="7" /></td>
-			<td>14:00</td>
-			<td><input type="checkbox" name="cT7" value="8" /></td>
-			<td>15:00</td>
-			<td><input type="checkbox" name="cT8" value="9" /></td>
-			<td>16:00</td>
-			<td><input type="checkbox" name="cT9" value="10" /></td>
-			<td>17:00</td>
-			<td><input type="checkbox" name="cT10" value="11" /></td>
-			<td>18:00</td>
-			<td><input type="checkbox" name="cT11" value="12" /></td>
-			<td>19:00</td>
-			<td><input type="checkbox" name="cT12" value="13" /></td>
-			<td>20:00</td>
-			<td><input type="checkbox" name="cT13" value="14" /></td>
-			<td>21:00</td>
-			<td><input type="checkbox" name="cT14" value="15" /></td>
-			<td>22:00</td>
-		</tr>
-		<tr>
-			<td><input type="checkbox" name="cDi" value="2" /></td>
-			<td>Dienstag</td>
-			<td></td>
-			<td><input type="checkbox" name="cT1" value="2" /></td>
-			<td>9:00</td>
-			<td><input type="checkbox" name="cT2" value="3" /></td>
-			<td>10:00</td>
-			<td><input type="checkbox" name="cT3" value="4" /></td>
-			<td>11:00</td>
-			<td><input type="checkbox" name="cT4" value="5" /></td>
-			<td>12:00</td>
-			<td><input type="checkbox" name="cT5" value="6" /></td>
-			<td>13:00</td>
-			<td><input type="checkbox" name="cT6" value="7" /></td>
-			<td>14:00</td>
-			<td><input type="checkbox" name="cT7" value="8" /></td>
-			<td>15:00</td>
-			<td><input type="checkbox" name="cT8" value="9" /></td>
-			<td>16:00</td>
-			<td><input type="checkbox" name="cT9" value="10" /></td>
-			<td>17:00</td>
-			<td><input type="checkbox" name="cT10" value="11" /></td>
-			<td>18:00</td>
-			<td><input type="checkbox" name="cT11" value="12" /></td>
-			<td>19:00</td>
-			<td><input type="checkbox" name="cT12" value="13" /></td>
-			<td>20:00</td>
-			<td><input type="checkbox" name="cT13" value="14" /></td>
-			<td>21:00</td>
-			<td><input type="checkbox" name="cT14" value="15" /></td>
-			<td>22:00</td>
-		</tr>
-		<tr>
-			<td><input type="checkbox" name="cDi" value="2" /></td>
-			<td>Mittwoch</td>
-			<td></td>
-			<td><input type="checkbox" name="cT1" value="2" /></td>
-			<td>9:00</td>
-			<td><input type="checkbox" name="cT2" value="3" /></td>
-			<td>10:00</td>
-			<td><input type="checkbox" name="cT3" value="4" /></td>
-			<td>11:00</td>
-			<td><input type="checkbox" name="cT4" value="5" /></td>
-			<td>12:00</td>
-			<td><input type="checkbox" name="cT5" value="6" /></td>
-			<td>13:00</td>
-			<td><input type="checkbox" name="cT6" value="7" /></td>
-			<td>14:00</td>
-			<td><input type="checkbox" name="cT7" value="8" /></td>
-			<td>15:00</td>
-			<td><input type="checkbox" name="cT8" value="9" /></td>
-			<td>16:00</td>
-			<td><input type="checkbox" name="cT9" value="10" /></td>
-			<td>17:00</td>
-			<td><input type="checkbox" name="cT10" value="11" /></td>
-			<td>18:00</td>
-			<td><input type="checkbox" name="cT11" value="12" /></td>
-			<td>19:00</td>
-			<td><input type="checkbox" name="cT12" value="13" /></td>
-			<td>20:00</td>
-			<td><input type="checkbox" name="cT13" value="14" /></td>
-			<td>21:00</td>
-			<td><input type="checkbox" name="cT14" value="15" /></td>
-			<td>22:00</td>
-		</tr>
-		<tr>
-			<td><input type="checkbox" name="cDi" value="2" /></td>
-			<td>Donnerstag</td>
-			<td></td>
-			<td><input type="checkbox" name="cT1" value="2" /></td>
-			<td>9:00</td>
-			<td><input type="checkbox" name="cT2" value="3" /></td>
-			<td>10:00</td>
-			<td><input type="checkbox" name="cT3" value="4" /></td>
-			<td>11:00</td>
-			<td><input type="checkbox" name="cT4" value="5" /></td>
-			<td>12:00</td>
-			<td><input type="checkbox" name="cT5" value="6" /></td>
-			<td>13:00</td>
-			<td><input type="checkbox" name="cT6" value="7" /></td>
-			<td>14:00</td>
-			<td><input type="checkbox" name="cT7" value="8" /></td>
-			<td>15:00</td>
-			<td><input type="checkbox" name="cT8" value="9" /></td>
-			<td>16:00</td>
-			<td><input type="checkbox" name="cT9" value="10" /></td>
-			<td>17:00</td>
-			<td><input type="checkbox" name="cT10" value="11" /></td>
-			<td>18:00</td>
-			<td><input type="checkbox" name="cT11" value="12" /></td>
-			<td>19:00</td>
-			<td><input type="checkbox" name="cT12" value="13" /></td>
-			<td>20:00</td>
-			<td><input type="checkbox" name="cT13" value="14" /></td>
-			<td>21:00</td>
-			<td><input type="checkbox" name="cT14" value="15" /></td>
-			<td>22:00</td>
-		</tr>
-		<tr>
-			<td><input type="checkbox" name="cDi" value="2" /></td>
-			<td>Freitag</td>
-			<td></td>
-			<td><input type="checkbox" name="cT1" value="2" /></td>
-			<td>9:00</td>
-			<td><input type="checkbox" name="cT2" value="3" /></td>
-			<td>10:00</td>
-			<td><input type="checkbox" name="cT3" value="4" /></td>
-			<td>11:00</td>
-			<td><input type="checkbox" name="cT4" value="5" /></td>
-			<td>12:00</td>
-			<td><input type="checkbox" name="cT5" value="6" /></td>
-			<td>13:00</td>
-			<td><input type="checkbox" name="cT6" value="7" /></td>
-			<td>14:00</td>
-			<td><input type="checkbox" name="cT7" value="8" /></td>
-			<td>15:00</td>
-			<td><input type="checkbox" name="cT8" value="9" /></td>
-			<td>16:00</td>
-			<td><input type="checkbox" name="cT9" value="10" /></td>
-			<td>17:00</td>
-			<td><input type="checkbox" name="cT10" value="11" /></td>
-			<td>18:00</td>
-			<td><input type="checkbox" name="cT11" value="12" /></td>
-			<td>19:00</td>
-			<td><input type="checkbox" name="cT12" value="13" /></td>
-			<td>20:00</td>
-			<td><input type="checkbox" name="cT13" value="14" /></td>
-			<td>21:00</td>
-			<td><input type="checkbox" name="cT14" value="15" /></td>
-			<td>22:00</td>
-		</tr>
-		<tr>
-			<td><input type="checkbox" name="cDi" value="2" /></td>
-			<td>Samstag</td>
-			<td></td>
-			<td><input type="checkbox" name="cT1" value="2" /></td>
-			<td>9:00</td>
-			<td><input type="checkbox" name="cT2" value="3" /></td>
-			<td>10:00</td>
-			<td><input type="checkbox" name="cT3" value="4" /></td>
-			<td>11:00</td>
-			<td><input type="checkbox" name="cT4" value="5" /></td>
-			<td>12:00</td>
-			<td><input type="checkbox" name="cT5" value="6" /></td>
-			<td>13:00</td>
-			<td><input type="checkbox" name="cT6" value="7" /></td>
-			<td>14:00</td>
-			<td><input type="checkbox" name="cT7" value="8" /></td>
-			<td>15:00</td>
-			<td><input type="checkbox" name="cT8" value="9" /></td>
-			<td>16:00</td>
-			<td><input type="checkbox" name="cT9" value="10" /></td>
-			<td>17:00</td>
-			<td><input type="checkbox" name="cT10" value="11" /></td>
-			<td>18:00</td>
-			<td><input type="checkbox" name="cT11" value="12" /></td>
-			<td>19:00</td>
-			<td><input type="checkbox" name="cT12" value="13" /></td>
-			<td>20:00</td>
-			<td><input type="checkbox" name="cT13" value="14" /></td>
-			<td>21:00</td>
-			<td><input type="checkbox" name="cT14" value="15" /></td>
-			<td>22:00</td>
-		</tr>
-		<tr>
-			<td><input type="checkbox" name="cDi" value="2" /></td>
-			<td>Sonntag</td>
-			<td></td>
-			<td><input type="checkbox" name="cT1" value="2" /></td>
-			<td>9:00</td>
-			<td><input type="checkbox" name="cT2" value="3" /></td>
-			<td>10:00</td>
-			<td><input type="checkbox" name="cT3" value="4" /></td>
-			<td>11:00</td>
-			<td><input type="checkbox" name="cT4" value="5" /></td>
-			<td>12:00</td>
-			<td><input type="checkbox" name="cT5" value="6" /></td>
-			<td>13:00</td>
-			<td><input type="checkbox" name="cT6" value="7" /></td>
-			<td>14:00</td>
-			<td><input type="checkbox" name="cT7" value="8" /></td>
-			<td>15:00</td>
-			<td><input type="checkbox" name="cT8" value="9" /></td>
-			<td>16:00</td>
-			<td><input type="checkbox" name="cT9" value="10" /></td>
-			<td>17:00</td>
-			<td><input type="checkbox" name="cT10" value="11" /></td>
-			<td>18:00</td>
-			<td><input type="checkbox" name="cT11" value="12" /></td>
-			<td>19:00</td>
-			<td><input type="checkbox" name="cT12" value="13" /></td>
-			<td>20:00</td>
-			<td><input type="checkbox" name="cT13" value="14" /></td>
-			<td>21:00</td>
-			<td><input type="checkbox" name="cT14" value="15" /></td>
-			<td>22:00</td>
-		</tr>
-	</table>
+  	
 	<p>Termine fuer eintragen fuer die folgenden naechsten Woche</p>
 
 	<form name=myform>

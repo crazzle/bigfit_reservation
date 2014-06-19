9795cbb357324f66aeeedb0d6701f6896ec71b30
superkeese <thedaab@googlemail.com>
2014-06-19 19:45:10 +0200

    new css

--- bigfit_reservation/reservationsystem/admin/newfile.php
+++ bigfit_reservation/reservationsystem/admin/newfile.php
@@ -12,42 +12,48 @@ include_once '../../includes/functions.php';
 
 <script type="text/JavaScript" src="js/sha512.js"></script>
 <script type="text/JavaScript" src="js/forms.js"></script>
-<link rel="stylesheet" type="text/css" href="../../css/Test.css">
+<link rel="stylesheet" type="text/css" href="../../css/bigfit_template.css">
 </head>
 <body>
-	<div class="Bild"></div>
-	<h1>Neue Trainingstermine anlegen</h1>
+	<div class= "Navigation_Top"></div>
+	<div class="MenueLeiste">
+		<table id="navMenue">
+			<tr> 
+				<td>
+				<a href="http://www.bigfitboxclub.de/">Startseite</a>
+				</td> <td>
+				<a href="admin/user_management.php">Mitgliederverwaltung</a>
+				</td> <td>
+				<a href="admin/appointment_management.php">Terminverwaltung</a>
+				</td> <td>
+				<a href="./includes/logout.php">Ausloggen</a>
+				</td>
+			</tr>
+		</table>
+		<div class= "Navigation_Botton"></div>
+		<div class="emotionheader"></div>	
+	</div>
+	
+
+	
+	<div class="content">
+		<h1>Neue Trainingstermine anlegen</h1>
 	
   	
-	<p>Termine fuer eintragen fuer die folgenden naechsten Woche</p>
+		<p>Termine fuer eintragen fuer die folgenden naechsten Woche</p>
 
-	<form name=myform>
-		<select name=mytextarea size=2>
-			<option value=one>1</option>
-			<option value=two>2</option>
-			<option value=three>3</option>
-			<option value=four>4</option>
-			<option value=four>5</option>
-			<option value=four>6</option>
-			<option value=four>7</option>
-			<option value=four>8</option>
-			<option value=four>9</option>
-			<option value=four>10</option>
-			<option value=four>11</option>
-			<option value=four>12</option>
-			<option value=four>13</option>
-			<option value=four>14</option>
-			<option value=four>15</option>
-			<option value=four>16</option>
-			<option value=four>17</option>
-			<option value=four>18</option>
-			<option value=four>19</option>
-			<option value=four selected>20</option>
+		<form name=myform>
+			<select name=mytextarea size=2>
+				<option value=one>1</option>
+				<option value=two>2</option>
+				<option value=three>3</option>
+				<option value=four>4</option>
+			</select>
+		</form>
 
-		</select>
-	</form>
-
-	<INPUT TYPE="Submit" Name="Submit1" VALUE="Termine eintragen">
+		<INPUT TYPE="Submit" Name="Submit1" VALUE="Termine eintragen">
+	</div>
+	
 	
 </body>
 

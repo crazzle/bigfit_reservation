52e283e8863332cbc6a518248f0dd40458245ccf
Mark K <mark.keinhoerster@gmail.com>
2014-07-08 18:09:35 +0200

    get rid of dedicated login page

--- bigfit_reservation/index.php
+++ bigfit_reservation/index.php
@@ -1,20 +1,48 @@
+<?php
+include_once 'includes/db_connect.php';
+include_once 'includes/functions.php';
+ 
+sec_session_start();
+ 
+if (login_check($mysqli) == true) {
+    $logged = 'in';
+    // Login erfolgreich
+    header ( 'Location: ./reservationsystem/reservation_index.php' );
+} else {
+    $logged = 'out';
+}
+?>
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title>BigFit Training Reservation System</title>
+        <title>Secure Login: Log In</title>
+        <link rel="stylesheet" href="styles/main.css" />
+        <script type="text/JavaScript" src="js/sha512.js"></script> 
+        <script type="text/JavaScript" src="js/forms.js"></script> 
+ 
+ 
         <link rel="stylesheet" type="text/css" href="css/bigfit_template.css">
     </head>
     <body>
-    	<div class= "Navigation_Top"></div>
-		<div class="MenueLeiste">
-			<div class= "Navigation_Botton"></div>
-			<div class="emotionheader"></div>
-		</div>
-    	<div class="content">	
-         <p>Zur&uumlck zum  <a href="login.php">Loginbereich</a></p>
-         <p>Zur&uumlck zur <a href="http://www.bigfitboxclub.de/">Startseite</a>
-         </div>
-         
+	    <?php include_once 'includes/navigationsleiste.php'; ?>
+	    
+	    <div class="content">
+	        <?php
+	        if (isset($_GET['error'])) {
+	            echo '<p class="error">Error Logging In!</p>';
+	        }
+	        ?> 
+	        <form action="includes/process_login.php" method="post" name="login_form">                      
+	            Email: <input type="text" name="email" />
+	            Password: <input type="password" 
+	                             name="password" 
+	                             id="password"/>
+	            <input type="submit" 
+	                   value="Login" 
+	                   onclick="formhash(this.form, this.form.password);" /> 
+	        </form>
+	        <p>You are currently logged <?php echo $logged ?>.</p>
+	    </div>
     </body>
+   
 </html>
